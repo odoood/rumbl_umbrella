1eefdfe34f71e7c75d8b6fa94041d04546fa7a11
odoood <2523476+odoood@users.noreply.github.com>
2020-09-28 22:28:43 -0400

    copy: cleanup rumbl app configs

--- apps/rumbl/mix.exs
+++ apps/rumbl/mix.exs
@@ -36,10 +36,9 @@ defmodule Rumbl.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix_pubsub, "~> 2.0"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:jason, "~> 1.0"}
+      {:pbkdf2_elixir, "~> 1.0"},
     ]
   end
 
