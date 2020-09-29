6f790bce757f4470be2a067fbdc21a52541073ed
odoood <2523476+odoood@users.noreply.github.com>
2020-09-29 19:07:20 -0400

    InfoSys: add Cache module & child spec for it to app

--- apps/info_sys/lib/info_sys/application.ex
+++ apps/info_sys/lib/info_sys/application.ex
@@ -7,6 +7,8 @@ defmodule InfoSys.Application do
 
   def start(_type, _args) do
     children = [
+      InfoSys.Cache,
+      {Task.Supervisor, name: InfoSys.TaskSupervisor},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
