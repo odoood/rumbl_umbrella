d2b778557dfb844ef871247638c83b6d5c537f6d
odoood <2523476+odoood@users.noreply.github.com>
2020-09-30 15:43:08 -0400

    Set app id config for Wolfram in dev & prod.secret

--- config/dev.exs
+++ config/dev.exs
@@ -74,3 +74,12 @@ config :phoenix, :plug_init_mode, :runtime
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
+
+# Set Wolfram API key
+wolfram_app_id =
+  System.get_env("WOLFRAM_APP_ID") ||
+    raise """
+    environment variable WOLFRAM_APP_ID is missing.
+    """
+    
+config :info_sys, :wolfram, app_id: wolfram_app_id
