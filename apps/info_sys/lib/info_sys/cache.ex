4ce214e311ff67c8036e07049853137e7a9f01b8
odoood <2523476+odoood@users.noreply.github.com>
2020-09-29 23:25:43 -0400

    InfoSys: mod Cache to clear itself after a timeout

--- apps/info_sys/lib/info_sys/cache.ex
+++ apps/info_sys/lib/info_sys/cache.ex
@@ -1,6 +1,27 @@
 defmodule InfoSys.Cache do
   use GenServer
 
+  @clear_interval :timer.seconds(60)
+
+  def init(opts) do
+    state = %{
+      interval: opts[:clear_interval] || @clear_interval,
+      timer: nil,
+      table: new_table(opts[:name])
+    }
+
+    {:ok, schedule_clear(state)}
+  end
+
+  def handle_info(:clear, state) do
+    :ets.delete_all_objects(state.table)
+    {:noreply, schedule_clear(state)}
+  end
+
+  defp schedule_clear(state) do
+    %{state | timer: Process.send_after(self(), :clear, state.interval)}
+  end
+
   def put(name \\ __MODULE__, key, value) do
     true = :ets.insert(tab_name(name), {key, value})
     :ok
@@ -15,11 +36,6 @@ defmodule InfoSys.Cache do
   def start_link(opts) do
     opts = Keyword.put_new(opts, :name, __MODULE__)
     GenServer.start_link(__MODULE__, opts, name: opts[:name])
-  end
-
-  def init(opts) do
-    new_table(opts[:name])
-    {:ok, %{}}
   end
 
   defp new_table(name) do
